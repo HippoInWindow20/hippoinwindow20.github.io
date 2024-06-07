bd63cc577a87f799ce4c4dcbc9fe47f91ee57ca6
HippoInWindow20 <shippo.hsu@gmail.com>
2024-06-07 22:39:14 +0800

    Restore the hateful words

--- www/content/work/default.js
+++ www/content/work/default.js
@@ -35,43 +35,43 @@ switch (jobDefined) {
         titleColour = blue;
         job = "體育股長";
         cname = "林○凡 同學";
-        desc = "屬於班上成績前幾名的認真好學生，也有參加排球隊。";
+        desc = "屬於班上成績前幾名的認真好學生，也有參加排球隊。同時是班上眾多宅宅的一份子。";
         break;
     case "general":
         titleColour = red;
         job = "總務股長";
         cname = "吳○瑄 同學";
-        desc = "也擔任化學小老師。樂於解決同學關於化學的疑難雜症。";
+        desc = "常會被一群男生圍在旁邊，聽她心情好會上的化學考前衝刺課。";
         break;
     case "test":
         titleColour = yellow;
         job = "試務股長";
         cname = "陳○均 同學";
-        desc = "負責去註冊組拿回成績確認單或悠遊卡註冊貼紙。";
+        desc = "班上宅宅之一，因有時和別人搞莫名的基情而管他叫噁男。但老師在課堂上抽到班上某個女同學也會中槍。";
         break;
     case "hygiene":
         titleColour = green;
         job = "衛生股長";
         cname = "古○宇 同學";
-        desc = "維護教室各個可能藏污納垢的小角落。在被班導的監督之下，班上的玻璃特別乾淨。";
+        desc = "和一些男生為伍，平時下課就不斷在亂搞每個女同學的水壺和水壺套。";
         break;
     case "eco":
         titleColour = blue;
         job = "環保股長";
         cname = "沈○寧 同學";
-        desc = "平時閑靜少言。有時老師提問某個歷史問題會不斷提問。";
+        desc = "平時閑靜少言，班上宅宅之一。有時老師提問某個歷史問題會無預警地說話。";
         break;
     case "recycle":
         titleColour = red;
         job = "資源回收股長";
         cname = "易○騰 同學";
-        desc = "平時除了管理資源回收區，還要照顧班上的共同信仰⎾瓜瓜。⏌";
+        desc = "平時除了管理資源回收區，還要照顧班上的共同信仰瓜瓜。";
         break;
     case "info":
         titleColour = yellow;
         job = "資訊股長";
         cname = "許○誠 同學";
-        desc = "班級網站編輯一員。見著學校班牌軟體的排版設計，自覺不滿意，便以此為自主學習計畫大刀闊斧地改造一番，結果還挺入得眼的。";
+        desc = "班級網站編輯一員。見著學校班牌軟體醜陋的排版設計後，便以此為自主學習計畫大刀闊斧地改造一番，結果還挺入得眼的。視電子班牌為其子，當表現不佳時他便會對它好好調教一下。認識更深之後就會發現他鮮為人知的另一面。";
         break;
     case "assist":
         titleColour = green;
@@ -83,19 +83,19 @@ switch (jobDefined) {
         titleColour = blue;
         job = "綠化股長";
         cname = "趙○凱 同學";
-        desc = "班上成績前段的一員，同時也是數學和物理大師。樂於上課提問，常常以同學不太能聽懂的語言與老師討論。";
+        desc = "除了也是成績前段的一員外，具備資深宅宅的各項能力。他願意的話就會突然跳上一段御宅藝。";
         break;
     case "covid":
         titleColour = red;
         job = "防疫股長";
         cname = "蔡○廣 同學";
-        desc = "負責放學後消毒同學桌面，疫情期間絕對不能少了他。";
+        desc = "負責放學後消毒同學桌面。有時也會被叫做噁男。";
         break;
     case "representative":
         titleColour = yellow;
         job = "班級代表";
         cname = "劉○祐、張○愷 同學";
-        desc = "負責參與學生會，代表班級表達意見。前者熟稔武術，且曾獲得多項國內獎項。後者是熱音社一員，看到熱音社學長的吉他表演便不斷想和他們看齊。";
+        desc = "負責參與學生會，代表班級表達意見。前者熟稔武術，試著創立武術社卻以不了了之告終。後者是熱音社一員，看到熱音社學長的吉他表演便不斷想和他們看齊。";
         break;
     default:
         titleColour = "black";
